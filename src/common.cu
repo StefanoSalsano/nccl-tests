7af856fbc551bed9bd8f6ef732a4ea488466afd3
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-06-14 11:59:04 +0200

    wip

--- src/common.cu
+++ src/common.cu
@@ -612,7 +612,7 @@ testResult_t TimeTest(struct threadArgs* args, ncclDataType_t type, const char* 
       setupArgs(size, type, args);
       char rootName[100];
       sprintf(rootName, "%6i", root);
-      PRINT("%12li  %12li  %8s  %6s  %6s", max(args->sendBytes, args->expectedBytes), args->nbytes / wordSize(type), typeName, opName, rootName);
+      PRINT("%12li  %12li  %8s  %6s  %6s *****\n", max(args->sendBytes, args->expectedBytes), args->nbytes / wordSize(type), typeName, opName, rootName);
       TESTCHECK(BenchTime(args, type, op, root, 0));
       TESTCHECK(BenchTime(args, type, op, root, 1));
       PRINT("\n");
