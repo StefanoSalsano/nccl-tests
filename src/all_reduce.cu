6aeca55d6b31cae7a42e574f0e28811bdb723b47
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-06-15 18:06:02 +0200

    wip

--- src/all_reduce.cu
+++ src/all_reduce.cu
@@ -41,6 +41,8 @@ void AllReduceGetBw(size_t count, int typesize, double sec, double* algBw, doubl
 }
 
 testResult_t AllReduceRunColl(void* sendbuff, void* recvbuff, size_t count, ncclDataType_t type, ncclRedOp_t op, int root, ncclComm_t comm, cudaStream_t stream) {
+  char hostname[1024];
+  getHostName(hostname, 1024);
   printf("%s:NCCL-TEST: AllReduceRunColl size: %12li \n",hostname,count);
   NCCLCHECK(ncclAllReduce(sendbuff, recvbuff, count, type, op, comm, stream));
   return testSuccess;
